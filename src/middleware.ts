4f6c903a997a4884f8de49874e76608d8995a2ae
Alex <fswoon2@gmail.com>
2024-06-15 09:32:13 -0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,27 @@
-import { verify } from "jsonwebtoken";
+import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
+async function isAuth(req: NextRequest) {
+  const nextPath = req.nextUrl.pathname;
+  const token = req.cookies.get("token");
 
-function isAuth(req: NextRequest) {
-
-    try {
-        const token = req.cookies.get('token')
-
-        if (token && typeof (token.value) === 'string') {
-            const decoded = verify(token.value, process.env.SECRET as string);
-            console.log(decoded)
-            if (decoded) {
-                console.log(decoded, req.nextUrl.pathname);
-                return NextResponse.redirect(new URL(req.nextUrl.pathname, req.url));
-            }
-        }
-
-        return NextResponse.redirect(new URL('/login', req.url));
-
-    } catch (err) {
-        console.error('Error: ', err);
-        return NextResponse.redirect(new URL('/login', req.url));
+  if (token && typeof token.value === "string") {
+    const secret = new TextEncoder().encode(process.env.SECRET);
+    const { payload, protectedHeader } = await jwtVerify(token.value, secret);
+    if (!payload && nextPath !== "/login") {
+      return NextResponse.redirect(new URL("/login", req.url));
+    } else if (payload["role"] === "admin") {
+      return NextResponse.next();
     }
+  } else if (nextPath !== "/login")
+    return NextResponse.redirect(new URL("/login", req.url));
 }
 
-
-export default function middleware(req: NextRequest) {
-    console.log("CHECK");
-    return isAuth(req);
+export default async function middleware(req: NextRequest) {
+  return await isAuth(req);
 }
-
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: '/salespersons',
-}
+  matcher: ["/salespersons", "/login"],
+};
