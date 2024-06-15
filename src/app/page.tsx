7da5403282d69ccae302ee16c2d26bc9a841534c
alexw005 <fswoon2@gmail.com>
2024-06-15 14:57:42 +0800

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Welcome to realty</h1>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 
       </div>
