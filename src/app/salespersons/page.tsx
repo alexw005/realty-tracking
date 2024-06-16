3953e60c095a2624c967f9a60da79bd781518bc4
Alex <fswoon2@gmail.com>
2024-06-16 02:35:15 -0700

    add realestate form

--- src/app/salespersons/page.tsx
+++ src/app/salespersons/page.tsx
@@ -1,22 +1,33 @@
-'use client'
+"use client";
 import { Input } from "@nextui-org/react";
 import { addSalesPersons } from "../server/actions";
 import SubmitButton from "../components/SubmitButton";
 
 export default function Page() {
-    return (
-        <main>
-            <div className="flex flex-col  p-24 items-center justify-between gap-4">
-                <h1 className="">Add Sales Person</h1>
-                <div>
-                    <form action={addSalesPersons}>
-
-                        <Input className='p-1' type="name" label="Name" name="name" required />
-                        <Input className='p-1' type="email" label="Email" name="email" required />
-                        <SubmitButton />
-                    </form>
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="flex flex-col  p-24 items-center justify-between gap-4">
+        <h1 className="">Add Salesperson</h1>
+        <div>
+          <form action={addSalesPersons}>
+            <Input
+              className="p-1"
+              type="name"
+              label="Name"
+              name="name"
+              required
+            />
+            <Input
+              className="p-1"
+              type="email"
+              label="Email"
+              name="email"
+              required
+            />
+            <SubmitButton />
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/app/server/actions.ts
+++ src/app/server/actions.ts
@@ -4,6 +4,57 @@ import prisma from "@/lib/db";
 import { SignJWT } from "jose";
 import { cookies } from "next/headers";
 
+export async function getAllSalesPersons() {
+  try {
+    const salesPersons = await prisma.salesPerson.findMany();
+    if (!salesPersons) {
+      return [];
+    }
+    return salesPersons;
+  } catch (e) {
+    return [];
+  }
+}
+export async function getAllRealEstates() {
+  try {
+    const realEstates = await prisma.realEstate.findMany();
+    if (!realEstates) {
+      return [];
+    }
+    return realEstates;
+  } catch (e) {
+    return [];
+  }
+}
+
+export async function getCommission() {
+  try {
+    const commissions = await prisma.commission.findMany();
+    if (!commissions) {
+      return [];
+    }
+    return commissions;
+  } catch (e) {
+    return [];
+  }
+}
+
+export async function getCommissionById(id: number) {
+  try {
+    const commission = await prisma.commission.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!commission) {
+      return [];
+    }
+    return commission;
+  } catch (e) {
+    return [];
+  }
+}
+
 export async function addSalesPersons(formData: FormData) {
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
@@ -16,6 +67,67 @@ export async function addSalesPersons(formData: FormData) {
     });
   } catch (e) {
     console.error(e);
+  }
+}
+export async function addRealEstate(formData: FormData) {
+  const name = formData.get("name") as string;
+  const address = formData.get("address") as string;
+  const price = parseFloat(formData.get("price") as string);
+  try {
+    await prisma.realEstate.create({
+      data: {
+        name,
+        address,
+        price,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function createCommission(formData: FormData) {
+  const salesPersonId = formData.get("salespersonid") as string;
+  const rate = formData.get("rate") as string;
+  const commissionId = await prisma.commission.findFirst({
+    include: {
+      salesPerson: true,
+      realEstate: true,
+    },
+    where: {
+      salesPersonId: parseInt(salesPersonId),
+      realEstateId: 1,
+    },
+  });
+  if (!commissionId) {
+    console.error("Commission not found");
+    return;
+  } else {
+    try {
+      const amount = commissionId.realEstate.price * parseFloat(rate);
+
+      console.log(commissionId.id, amount);
+      await prisma.commission.upsert({
+        where: {
+          // in this case, the combination of salesPersonId and realEstateId
+          id: commissionId.id,
+          salesPersonId: parseInt(salesPersonId),
+          realEstateId: 1,
+        },
+        create: {
+          amount: amount,
+          rate: parseFloat(rate),
+          realEstateId: 1,
+          salesPersonId: parseInt(salesPersonId),
+        },
+        update: {
+          amount: amount,
+          rate: parseFloat(rate),
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
 export async function login(formData: FormData) {
