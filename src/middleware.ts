e0fd7f1912ab93a25bf1a155dc26370900e597c1
Alex <fswoon2@gmail.com>
2024-06-15 19:47:34 -0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,22 @@ import { NextRequest, NextResponse } from "next/server";
 async function isAuth(req: NextRequest) {
   const nextPath = req.nextUrl.pathname;
   const token = req.cookies.get("token");
-
-  if (token && typeof token.value === "string") {
-    const secret = new TextEncoder().encode(process.env.SECRET);
-    const { payload, protectedHeader } = await jwtVerify(token.value, secret);
-    if (!payload && nextPath !== "/login") {
+  try {
+    if (token && typeof token.value === "string") {
+      const secret = new TextEncoder().encode(process.env.SECRET);
+      const { payload, protectedHeader } = await jwtVerify(token.value, secret);
+      if (!payload && nextPath !== "/login") {
+        return NextResponse.redirect(new URL("/login", req.url));
+      } else if (payload["role"] === "admin") {
+        return NextResponse.next();
+      }
+    } else if (nextPath !== "/login") {
       return NextResponse.redirect(new URL("/login", req.url));
-    } else if (payload["role"] === "admin") {
-      return NextResponse.next();
     }
-  } else if (nextPath !== "/login")
+  } catch (e) {
+    console.error("Failed to login with errors: ", e);
     return NextResponse.redirect(new URL("/login", req.url));
+  }
 }
 
 export default async function middleware(req: NextRequest) {
@@ -23,5 +28,5 @@ export default async function middleware(req: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/salespersons", "/login"],
+  matcher: ["/salespersons"],
 };
